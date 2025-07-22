8a2747f4da282a6e64329e1992b20e0dc8beda05
Abul Hassan <imabulhasan99@gmail.com>
2025-07-23 00:35:35 +0600

    feat(test_smtp): extend SMTP test script with RCPT and DATA command handling
    - Add functionality to send RCPT TO and DATA commands in the SMTP test script
    - Implement message composition with From, To, and Subject headers
    - Enhance response logging for better visibility of server interactions

--- test_smtp.php
+++ test_smtp.php
@@ -27,6 +27,35 @@ while($line = fgets($socket, 512)) {
     if(substr($line, 3, 1) === ' ') break;
 }
 
+// Send MAIL TO command
+
+fwrite($socket, "RCPT TO: <hasanboronal@gmail.com>\r\n");
+echo "Client: RCPT TO: <hasanboronal@gmail.com>\n";
+
+//Read response
+while($line = fgets($socket, 512)) {
+    echo "Server: " . $line;
+    if(substr($line, 3, 1) === ' ') break;
+}
+
+// Send DATA commad
+fwrite($socket, "DATA\r\n");
+echo "Client: DATA\n";
+
+//Read response
+while($line = fgets($socket, 512)) {
+    echo "Server: " . $line;
+    if(substr($line, 3, 1) === ' ') break;
+}
+
+//Send message
+fwrite($socket, "From: test@test.com\r\n");
+fwrite($socket, "To: hasanboronal@gmail.com\r\n");
+fwrite($socket, "Subject: Test message\r\n");
+fwrite($socket, "\r\n");
+fwrite($socket, "This is a test message\r\n");
+fwrite($socket, ".\r\n");
+
 // Send QUIT
 fwrite($socket, "QUIT\r\n");
 echo "Client: QUIT\n";
